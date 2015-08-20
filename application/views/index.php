63f765c16b361917086b6a2242d8ee0b184efa71
mikesoriano <michael.soriano@live.com>
2015-08-19 23:12:53 -0700

    first commit

--- application/views/index.php
+++
@@ -1,5 +0,0 @@
-<h1>Welcome to the Home Page!</h1>
-<h3>Your name is: <?php echo($first_name . ' ' . $last_name)?></h3>
-<p>Your email is: <?php echo($email);?></p>
-<p>You last logged in on: <?php echo($last_login);?></p>
-<a href="<?php echo site_url().'main/logout';?>">Logout</a>
